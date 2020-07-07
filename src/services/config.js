14fcf361a8277ee551f3c696f0f19a26bca42263
Gabriel Nadaleti <37385450+Byttar@users.noreply.github.com>
2020-07-07 11:44:20 -0300

    Removing my key

--- src/services/config.js
+++ src/services/config.js
@@ -1,14 +1,7 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC01v5GHcSKKTHdRMfrHdJGcxlIZVUN2nk",
-    authDomain: "trellinho.firebaseapp.com",
-    databaseURL: "https://trellinho.firebaseio.com",
-    projectId: "trellinho",
-    storageBucket: "trellinho.appspot.com",
-    messagingSenderId: "666173448044",
-    appId: "1:666173448044:web:ec225fa0fca8b2dfce1d49",
-    measurementId: "G-NXY3X0MJS6"
+       //config
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
